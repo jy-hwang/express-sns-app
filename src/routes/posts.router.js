264c30aa3487086d891ad92db9f45ee5a5fbb199
jy-hwang <stratojy@gmail.com>
2024-08-04 15:02:30 +0900

    feat. 이미지 업로드 및 포스트 생성하기
    1. 포스트 생성 쪽 css가 작성되지 않은 부분 처리.
    2. 이미지를 업로드할때, multer 라이브러리 이용.
    3. local disk 에 바로 저장처리.
    4. 포스트 저장하는 로직 추가.

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,7 +1,42 @@
 const express = require('express');
+const multer = require('multer');
 const { checkAuthenticated } = require('../middleware/auth');
 const router = express.Router();
 const Post = require('../models/posts.model');
+const path = require('path');
+const Comment = require('../models/comments.model');
+
+const storageEngine = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, '../public/assets/images'));
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageEngine }).single('image');
+
+router.post('/', checkAuthenticated, upload, (req, res, next) => {
+  let desc = req.body.desc;
+  let image = req.file ? req.file.filename : '';
+
+  Post.create({
+    image: image,
+    description: desc,
+    author: {
+      id: req.user._id,
+      username: req.user.username,
+    },
+  })
+    .then(post => {
+      res.redirect('back');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 router.get('/', checkAuthenticated, (req, res) => {
   Post.find()
     .populate('comments')
