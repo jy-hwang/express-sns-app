f6beb134f102a41ef669d02f22e740825aac8bd9
jy-hwang <stratojy@gmail.com>
2024-08-04 19:54:24 +0900

    feat. 포스트 삭제
    1. 포스트 삭제 핸들러 등록
    2. 소스 리팩토링

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -5,9 +5,24 @@ const {
   checkPostOwnerShip,
 } = require('../middleware/auth');
 const router = express.Router();
-const Post = require('../models/posts.model');
 const path = require('path');
+
+const Post = require('../models/posts.model');
 const Comment = require('../models/comments.model');
+
+router.get('/', checkAuthenticated, (req, res) => {
+  Post.find()
+    .populate('comments')
+    .sort({ createdAt: -1 })
+    .then(posts => {
+      res.render('posts', {
+        posts: posts,
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 
 // 파일 업로드 위치 변경
 const uploadDir = path.join(__dirname, '..', 'uploads', 'images');
@@ -36,26 +51,13 @@ router.post('/', checkAuthenticated, upload, (req, res, next) => {
     },
   })
     .then(post => {
-      req.flash('success', '포스트 생성 성공');
-      res.redirect('back');
+      req.flash('success', '게시물 생성을 성공했습니다.');
     })
     .catch(err => {
-      req.flash('error', '포스트 생성 실패');
-      res.redirect('back');
-    });
-});
-
-router.get('/', checkAuthenticated, (req, res) => {
-  Post.find()
-    .populate('comments')
-    .sort({ createdAt: -1 })
-    .then(posts => {
-      res.render('posts', {
-        posts: posts,
-      });
+      req.flash('error', '게시물 생성을 실패했습니다.');
     })
-    .catch(err => {
-      console.error(err);
+    .finally(() => {
+      res.redirect('/posts');
     });
 });
 
@@ -69,10 +71,24 @@ router.put('/:id', checkPostOwnerShip, (req, res) => {
   Post.findByIdAndUpdate(req.params.id, req.body)
     .then(post => {
       req.flash('success', '게시물 수정을 완료했습니다.');
-      res.redirect('/posts');
     })
     .catch(err => {
       req.flash('error', '게시물을 수정하는 중에 오류가 발생했습니다.');
+    })
+    .finally(() => {
+      res.redirect('/posts');
+    });
+});
+
+router.delete('/:id', checkPostOwnerShip, (req, res) => {
+  Post.findByIdAndDelete(req.params.id)
+    .then(post => {
+      req.flash('success', '게시물 삭제를 완료했습니다.');
+    })
+    .catch(err => {
+      req.flash('error', '게시물을 삭제하는 중에 오류가 발생했습니다.');
+    })
+    .finally(() => {
       res.redirect('/posts');
     });
 });
