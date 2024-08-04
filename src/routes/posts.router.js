2978085a74583d3c6c69d5b9496884ef5fe5cf8f
jy-hwang <stratojy@gmail.com>
2024-08-04 14:07:22 +0900

    feat. 포스트 UI 생성하기
    1. post 생성하는 페이지 수정
    2. modal 부분도 따로 처리.
    3. post 페이지에 들어올 때, post 와 그에 달린 댓글을 가져옴.
    4. currentUser 가 passport 처리 후 req.user 에 정보다 담겨져 있음.

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const { checkAuthenticated } = require('../middleware/auth');
 const router = express.Router();
-
+const Post = require('../models/posts.model');
 router.get('/', checkAuthenticated, (req, res) => {
-  res.render('posts');
+  Post.find()
+    .populate('comments')
+    .sort({ createdAt: -1 })
+    .then(posts => {
+      res.render('posts', {
+        posts: posts,
+        currentUser: req.user,
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 module.exports = router;
