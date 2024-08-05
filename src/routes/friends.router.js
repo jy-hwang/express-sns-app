ce255949f83c78a337cfebbc1ea1585594e579cb
jy-hwang <stratojy@gmail.com>
2024-08-05 20:38:27 +0900

    feat. Friends 페이지 & UI 생성하기
    1. /friends/ 진입하는 get 라우터 생성
    2. Friends 필터링조건
    1) 친구요청중인가?
    2) 이미 친구인가?
    3) 알 수도 있는 사람 : ( 나 !== 상대방 && !친구요청중 && !이미친구 )

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const { checkAuthenticated } = require('../middleware/auth');
 const router = express.Router();
+const User = require('../models/users.model');
+
+router.get('/', checkAuthenticated, (req, res) => {
+  User.find()
+    .then(users => {
+      res.render('friends', {
+        users: users,
+      });
+    })
+    .catch(err => {
+      req.flash('error', '유저 목록을 가져오는데 에러가 발생했습니다.');
+      res.redirect('/posts');
+    });
+});
 
 module.exports = router;
