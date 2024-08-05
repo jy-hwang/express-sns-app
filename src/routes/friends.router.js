695254764a279d6994b4a9d8bdef6735ea435399
jy-hwang <stratojy@gmail.com>
2024-08-05 21:38:56 +0900

    feat. Friends 페이지 기능 생성하기
    1. 페이지에 오타 및 제대로 매핑되지 않은 부분수정
    2. 친구 요청 및 친구 요청 취소 기능 생성

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -11,9 +11,59 @@ router.get('/', checkAuthenticated, (req, res) => {
       });
     })
     .catch(err => {
-      req.flash('error', '유저 목록을 가져오는데 에러가 발생했습니다.');
+      req.flash('error', '유저 목록을 가져오는데 오류가 발생했습니다.');
       res.redirect('/posts');
     });
 });
+
+router.put('/:id/add-friend', checkAuthenticated, (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      // 상대방 유저의 friendsRequests 에 나의 아이디를 추가
+      User.findByIdAndUpdate(user._id, {
+        friendsRequests: user.friendsRequests.concat([req.user._id]),
+      })
+        .then(user => {
+          req.flash('success', '친구 추가 요청을 성공했습니다.');
+          res.redirect('back');
+        })
+        .catch(err => {
+          req.flash('error', '친구 추가 요청을 하는데 오류가 발생했습니다.');
+          res.redirect('back');
+        });
+    })
+    .catch(err => {
+      req.flash('error', '유저가 없거나 유저를 찾는데 오류가 발생했습니다.');
+      res.redirect('back');
+    });
+});
+
+router.put(
+  '/:firstId/remove-friend-request/:secondId',
+  checkAuthenticated,
+  (req, res) => {
+    User.findById(req.params.firstId)
+      .then(user => {
+        const filteredFriendsRequests = user.friendsRequests.filter(
+          friendId => friendId !== req.params.secondId,
+        );
+        User.findByIdAndUpdate(user._id, {
+          friendsRequests: filteredFriendsRequests,
+        })
+          .then(user => {
+            req.flash('success', '친구 요청 거절했습니다.');
+            res.redirect('back');
+          })
+          .catch(err => {
+            req.flash('error', '친구 요청을 거절하는데 오류가 발생했습니다.');
+            res.redirect('back');
+          });
+      })
+      .catch(err => {
+        req.flash('error', '유저를 찾지 못했습니다.');
+        res.redirect('back');
+      });
+  },
+);
 
 module.exports = router;
