520a9cc18804769c4034426d7d79a55212de6b92
jy-hwang <stratojy@gmail.com>
2024-08-11 15:04:32 +0900

    Profile 페이지 데이터 생성하기
    1. router 생성
    2. 결과 데이터가 잘 넘어가는지 확인

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -1,4 +1,34 @@
 const express = require('express');
-const router = express.Router();
+const { checkAuthenticated } = require('../middleware/auth');
+const router = express.Router({
+  mergeParams: true,
+});
+const Post = require('../models/posts.model');
+const User = require('../models/users.model');
+router.get('/', checkAuthenticated, (req, res) => {
+  Post.find({ 'author.id': req.params.id })
+    .populate('comments')
+    .sort({ createdAt: -1 })
+    .then(posts => {
+      User.findById(req.params.id)
+        .then(user => {
+          res.render('profile', {
+            posts: posts,
+            user: user,
+          });
+        })
+        .catch(err => {
+          req.flash('error', '없는 사용자 입니다.');
+          res.redirect('/back');
+        });
+
+      req.flash;
+    })
+    .catch(err => {
+      console.error(err);
+      req.flash('error', '게시물을 가져오는 데에 실패했습니다.');
+      res.redirect('/back');
+    });
+});
 
 module.exports = router;
